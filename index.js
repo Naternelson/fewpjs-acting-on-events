cc29debe7ca8acd9691aa4aa08072e3617f2cef7
Nate <7nater.nelson@gmail.com>
2021-04-12 13:34:00 -0600

    commit tree 2f0860e12588784151b49763ff8e0109ba45eb7b

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+let dodger = document.getElementById("dodger"); 
+
+function parseStyle(style){
+    return parseInt(style.replace("px", ""), 10);
+}
+
+function moveDodgerLeft() {
+    let left = parseStyle(dodger.style.left);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+} 
+function moveDodgerRight() {
+    let right = parseStyle(dodger.style.left);
+
+    dodger.style.left = `${right + 1}px`;
+} 
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+      }
+  }); 
